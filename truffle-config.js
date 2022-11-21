5d2a355e6f8c1b665eea7085260f322342960ec6
Aditya Ghaffar <adit.bisa@gmail.com>
2022-11-22 00:20:07 +0700

    feat: connect to Windows Host

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,17 @@
+var os = require("os");
+
+// Dev host in my WSL2
+var devHost = "127.0.0.1";
+if (os.homedir().startsWith("/home/adit")) {
+  devHost = os.hostname() + ".local";
+}
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: devHost,
       port: 7545,
       network_id: "*" // Match any network id
     },
