0b9316b8e87a02793316c14eddf331d34736103e
Aditya Ghaffar <adit.bisa@gmail.com>
2022-11-22 01:25:47 +0700

    test: adopt pet

--- test/test_adoption.js
+++ test/test_adoption.js
@@ -5,11 +5,34 @@ const TestAdoption = artifacts.require("Adoption");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("TestAdoption", function (/* accounts */) {
+contract("TestAdoption", function (accounts) {
   let adoption;
 
-  it("should assert true", async function () {
+  before("get the contract", async function () {
     adoption = await TestAdoption.deployed();
-    return assert.isTrue(true);
+  });
+
+  it("accounts[0] can adopt a pet index 8", async () => {
+    await adoption.adopt(8, { from: accounts[0] });
+  });
+
+  it("accounts[0] cannot adopt a pet index -2", async () => {
+    let error;
+    try {
+      await adoption.adopt(-2, { from: accounts[0] });
+    } catch (err) {
+      error = err;
+    }
+    assert.instanceOf(error, Error);
+  });
+
+  it("accounts[0] cannot adopt a pet index 30", async () => {
+    let error;
+    try {
+      await adoption.adopt(30, { from: accounts[0] });
+    } catch (err) {
+      error = err;
+    }
+    assert.instanceOf(error, Error);
   });
 });
